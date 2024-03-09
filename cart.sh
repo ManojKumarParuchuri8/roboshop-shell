b9389b9f5dfc21674028ae78c231fe90e8742413
manoj <manojkumar.paruchuri532@gmail.com>
2024-03-09 18:01:29 +0530

    cart file is added

--- cart.sh
+++ cart.sh
@@ -41,7 +41,7 @@ dnf install nodejs -y &>> $LOGFILE
 VALIDATE $? "Installing NodeJS"
 
 id roboshop
-if[ $? -ne 0 ]
+if [ $? -ne 0 ]
 then 
     useradd roboshop
     VALIDATE $? "Roboshop User Creation"
